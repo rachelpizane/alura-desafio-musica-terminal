6c57e621ed3a9e0e2e2794cae2cc6cdcf7f6efc1
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-05-08 11:38:54 -0300

    first commit

---
+++ src/main/java/edu/rachelpizane/api_musica_terminal/ApiMusicaTerminalApplication.java
@@ -0,0 +1,13 @@
+package edu.rachelpizane.api_musica_terminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMusicaTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMusicaTerminalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/rachelpizane/api_musica_terminal/ApiMusicaTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rachelpizane.api_musica_terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMusicaTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
