de031ab90cc21947dd919cd9c8cd804f8ceba869
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-05-08 12:08:29 -0300

    feat: implementada a classe Principal com menu interativo para gerenciamento de músicas e artistas

--- src/main/java/edu/rachelpizane/api_musica_terminal/ApiMusicaTerminalApplication.java
+++ src/main/java/edu/rachelpizane/api_musica_terminal/ApiMusicaTerminalApplication.java
@@ -1,13 +1,24 @@
 package edu.rachelpizane.api_musica_terminal;
 
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ApiMusicaTerminalApplication {
+import edu.rachelpizane.api_musica_terminal.principal.Principal;
 
+@SpringBootApplication
+public class ApiMusicaTerminalApplication implements CommandLineRunner {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiMusicaTerminalApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(String... args){
+		System.out.println("====== API DE MUSICA - TERMINAL ======");
+		Principal.exibirMenu();
+		System.out.println("====== [FIM] API DE MUSICA - TERMINAL ======");
+	}
 }
---
+++ src/main/java/edu/rachelpizane/api_musica_terminal/principal/Principal.java
@@ -0,0 +1,81 @@
+package edu.rachelpizane.api_musica_terminal.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void exibirMenu() {
+        String opcao = "";
+
+        while (!opcao.equals("x")) {
+            scanner = new Scanner(System.in);
+            String menu = """
+                    - MENU ----
+                    1 - Cadastar artistas
+                    2 - Cadastrar música
+                    3 - Listar todas as músicas
+                    4 - Listar músicas por artista
+                    5 - Buscar informações sobre o artista
+
+                    x - Sair da Aplicação
+                    """;
+            String inputMensagem = "Digite a opção desejada: ";
+
+            System.out.println(menu);
+            System.out.print(inputMensagem);
+            opcao = scanner.nextLine();
+            // opcao = "x";
+            System.out.println("\n");
+
+            switch (opcao) {
+                case "x":
+                    System.out.println(">> Obrigada por usar a API!");
+                    break;
+                case "1":
+                    cadastrarArtista();
+                    break;
+                case "2":
+                    cadastrarMusica();
+                    break;
+                case "3":
+                    listarTodasMusicas();
+                    break;
+                case "4":
+                    listarMusicasPorArtista();
+                    break;
+                case "5":
+                    pesquisarDadosSobreArtista();
+                    break;
+                default:
+                    System.out.println(">> Opção inválida.Tente novamente.");
+                    break;
+            }
+        }
+    }
+
+    private static void listarTodasMusicas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarTodasMusicas'");
+    }
+
+    private static void pesquisarDadosSobreArtista() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pesquisarDadosSobreArtista'");
+    }
+
+    private static void listarMusicasPorArtista() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarMusicasPorArtista'");
+    }
+
+    private static void cadastrarMusica() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastrarMusica'");
+    }
+
+    private static void cadastrarArtista() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastrarArtista'");
+    }
+}
